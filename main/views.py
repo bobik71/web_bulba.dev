6dcc2b1e11b43f2e194927168dcb295d265bfced
Arseniy <senapronchak71@gmail.com>
2025-07-25 12:12:03 +0300

    переименование главного файла изза чего не работал сайт, добавление html шаблонов

--- main/views.py
+++ main/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h4>Проверка работы/<h4>")
+    return render(request, 'main/index.html')
 
 def about(request):
-    return HttpResponse("<h4>Про нас/<h4>")
+    return render(request, 'main/about.html')
