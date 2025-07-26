f788bf5c5e57eb06344335975485441484be6966
Arseniy <senapronchak71@gmail.com>
2025-07-26 15:58:27 +0300

    добавление передачи данных в шаблоны

--- main/urls.py
+++ main/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.index, name='home'),
-    path('about', views.about, name='about')
+    path('home/', views.index, name='home'),       # главная страница на корневой URL
+    path('about/', views.about, name='about')
 ]
 
 
