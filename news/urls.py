50519df9f3de44b6fa281f850354be0def283939
Arseniy <senapronchak71@gmail.com>
2025-07-30 20:19:25 +0300

    добавление кнопки подробного прочтения комментария и перехода на его страницу(динамически изменяемые страницы)(прошлый коммит залагал)

--- news/urls.py
+++ news/urls.py
@@ -6,4 +6,5 @@ app_name = 'news'
 urlpatterns = [
     path('', news_views.news_home, name='news_home'),
     path('create', news_views.create, name='create'),
+    path('<int:pk>', news_views.NewsDetailView.as_view(), name='news-detail')
 ]
