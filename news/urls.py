5e6bbb3973491b07f13b5c51172985977f45d8ed
Arseniy <senapronchak71@gmail.com>
2025-07-30 21:24:08 +0300

    добавление и изменение новостей

--- news/urls.py
+++ news/urls.py
@@ -6,5 +6,7 @@ app_name = 'news'
 urlpatterns = [
     path('', news_views.news_home, name='news_home'),
     path('create', news_views.create, name='create'),
-    path('<int:pk>', news_views.NewsDetailView.as_view(), name='news-detail')
+    path('<int:pk>', news_views.NewsDetailView.as_view(), name='news-detail'),
+    path('<int:pk>/update', news_views.NewsUpdateView.as_view(), name='news-update'),
+    path('<int:pk>/delete', news_views.NewsDeleteView.as_view(), name='news-delete'),
 ]
